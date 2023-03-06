9971250695bbee75b1d823cc07a4f8b91a683797
Henrik Hviid <au591994@uni.au.dk>
2023-03-06 16:15:25 +0100

    bimebamle

--- Simulation.py
+++ Simulation.py
@@ -21,15 +21,47 @@ class Network(object):
     def __init__(self, env) -> None:
         self.env = env
         self.nodes = []
- 
-class Node(object):
-    def __init__(self, env, pos, parent, radius) -> None:
+
+class MessageType(object):
+    def __init__(self) -> None:
+        self.test = 1 
+
+class NodeConnection(object):
+    def __init__(self, env) -> None:
         self.env = env
+        self.connection = simpy.Store(self.env, capacity=10)
+
+    def sendMessage(self) -> None:
+        test = MessageType()
+        self.connection.put(test)
+
+    def readMessage(self) -> MessageType:
+        msg = yield self.connection.get()
+        print(msg.test)
+        return msg
+
+
+class Node(object):
+    def __init__(self, env, pos, parent, radius, id) -> None:
+        self.env = env
+        self.id = id
         self.pos = pos
         self.parent = parent
         self.siblings = []
         self.children = []
+        self.connections = {}
+        self.routingTable = {}
         self.radioRadius = radius
+
+    def addConnection(self, node):
+        connection = NodeConnection(self.env)
+        self.connections[f'{node.id}'] = connection
+        node.connections[f'{self.id}'] = connection
+
+    def alive(self):
+        while True:
+            yield self.env.timeout(5)
+            pass 
 
 class RootNode(Node,object):
     def __init__(self, env, pos, parent, radius) -> None:
@@ -43,7 +75,14 @@ class RootNode(Node,object):
 
 def main():
     env = simpy.Environment()
-    test = TestClass(env)
+    
+    node1 = Node(env, (1,2) , None, 2, 1)
+    node2 = Node(env, (3,5) , None, 2, 2)
+
+    node1.addConnection(node2)
+    node1.connections['2'].sendMessage()
+    env.process(node2.connections['1'].readMessage())
+
     env.run(until=50)
     
 
